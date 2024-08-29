d4d6b3b35040b6446057bc98105b19f931d5c8bc
anshalc <anshalc.py@gmail.com>
2024-08-29 12:33:01 -0700

    Added Github Actions workflow for Testing

--- Basic Math Functions/BMF.py
+++ Basic Math Functions/BMF.py
@@ -1,31 +1,46 @@
 # START HERE
 
-import math
+class MathOperations:
+    
+    # Constructor with an Argument
+    def  __init__(self, num=0):
+        self.num = num
 
-# MAIN FUNCTION
-def main():
-    try:
-        rtn_value = float(input("Enter a Decimal Number: "))
-
-        sml_int = int(math.floor(rtn_value))  # Largest integer smaller than the number
-        rnd_int = round(rtn_value)            # Nearest integer to the number
-        lrg_int = int(math.ceil(rtn_value))   # Smallest integer larger than the number
-
-        print("The floating number you entered is:             \n", rtn_value)
-        # prints the floating number entered by the user.
-
-        print("The largest integer smaller than the number is: \n", sml_int)
-        # prints the integer smaller than the floating number.
-
-        print("The nearest integer to the number is:           \n", rnd_int)
-        # prints the nearest integer to the value, with halfway values rounded away from zero.
-
-        print("The smallest integer larger than the number is: \n", lrg_int)
-        # prints the integer greater than the floating number.
-
-    except ValueError:
-        # if input is not a valid floating-point number.
-        print("OOPSIE!! You did not enter a valid floating number\n")
-
-if __name__ == "__main__":
-    main()
+    # Round Up Function Comparable to math.ceil
+    def roundUp(self):
+        if(self.num > 0):
+            if(self.num > int(self.num)):
+               return int(self.num) + 1
+            else:
+                return int(self.num)
+        else:
+            if(self.num < int(self.num)):
+               return int(self.num)
+            else:
+                return int(self.num)
+            
+    # Round Down Function Comparable to math.floor
+    def roundDown(self):
+        if(self.num > 0):
+            if(self.num > int(self.num)):
+               return int(self.num)
+            else:
+                return int(self.num)
+        else:
+            if(self.num < int(self.num)):
+               return int(self.num) - 1
+            else:
+                return int(self.num)
+            
+    # Round Function Comparable to round
+    def justRound(self):
+        if(self.num > 0):
+            if(self.num > float(int(self.num) + 0.5)):
+               return int(self.num) + 1
+            else:
+                return int(self.num)
+        else:
+            if(self.num > float(int(self.num) - 0.5)):
+               return int(self.num)
+            else:
+                return int(self.num) - 1
--- Basic Math Functions/Test_BMF.py
+++ Basic Math Functions/Test_BMF.py
@@ -2,10 +2,27 @@
 
 import unittest
 import math
-from Basic_Math_Functions import main
+from BMF import MathOperations
 
-class TestMainProgram(unittest.TestCase):
+class TestMathOperations(unittest.TestCase):
 
-    def test_floor(self):
-        # Test case for math.floor()
-        self.assertEqual(math.floor(3.7), 3)
+    def test_roundUp(self):
+        self.assertEqual(MathOperations(-1.2).roundUp(), math.ceil(-1.2))
+        self.assertEqual(MathOperations().roundUp(), 0)
+        self.assertEqual(MathOperations(10000000000.2).roundUp(), math.ceil(10000000000.2))
+        self.assertEqual(MathOperations(-10000000000.2).roundUp(), math.ceil(-10000000000.2))
+    
+    def test_roundDown(self):
+        self.assertEqual(MathOperations(-1.2).roundDown(), math.floor(-1.2))
+        self.assertEqual(MathOperations().roundDown(), 0)
+        self.assertEqual(MathOperations(10000000000.2).roundDown(), math.floor(10000000000.2))
+        self.assertEqual(MathOperations(-10000000000.2).roundDown(), math.floor(-10000000000.2))
+    
+    def test_justRound(self):
+        self.assertEqual(MathOperations(-1.2).justRound(), round(-1.2))
+        self.assertEqual(MathOperations().justRound(), 0)
+        self.assertEqual(MathOperations(10000000000.2).justRound(), round(10000000000.2))
+        self.assertEqual(MathOperations(-10000000000.6).justRound(), round(-10000000000.6))
+
+if __name__ == "__main__":
+    unittest.main()
